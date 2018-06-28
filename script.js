d0619193e98b5d392c989fe02784e6433bd16438
Glitch (15-sisisin) <none>
2018-06-28 13:53:28 +0000

    💈☔️ Checkpoint ./script.js:641133/124

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 jQuery(async $ => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 4, 4]).map(Number);
   const table = [];
+
   for (let y = 0; y < height; y++) {
     const row = [];
-    table.push(row);
+
     for (let x = 0; x < width ; x++) {
-      row.push();
+      row.push({ x, y, last: (x + 1) * (y + 1)  });
     }
+
+    table.push(row);
   }
+
   // const nyan = $(`<div class="nyan" data-x="${x}" data-y="${y}">`);
 });
