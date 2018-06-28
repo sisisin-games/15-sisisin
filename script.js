4b1c896d67d0409c38d380ac72e3e4d021b8ff56
Glitch (15-sisisin) <none>
2018-06-28 14:04:05 +0000

    🎰👓 Checkpoint ./script.js:641133/426

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$(document).on('click', '.nyan', ({target}) => {});
+
 jQuery(async $ => {
   const [, width, height] = (location.search.match(/\bsize=(\d+)x(\d+)/) || [, 4, 4]).map(Number);
   const table = [];
@@ -6,11 +8,14 @@ jQuery(async $ => {
     const row = [];
 
     for (let x = 0; x < width ; x++) {
-      row.push({ x, y, last: (x + 1) * (y + 1)  });
+      row.push({
+        x,
+        y,
+        nyan: $(`<div class="nyan" data-x="${x}" data-y="${y}">`),
+        last: x + 1 === width && y + 1 === height,
+      });
     }
 
     table.push(row);
   }
-
-  // const nyan = $(`<div class="nyan" data-x="${x}" data-y="${y}">`);
 });
